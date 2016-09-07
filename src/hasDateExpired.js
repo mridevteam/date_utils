2ab2fe88b3b754efbba5522dd1e93e0981f6a94a
Mike Fielden <fielden.mike@gmail.com>
2016-09-07 10:18:16 -0500

    hasDateExpired

---
+++ src/hasDateExpired.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const validityChecks = require('validity_checks')
+  , reqParam = validityChecks.requiredParameter
+  , isDate = validityChecks.isDate
+  ;
+
+/**
+ * Determines if a date has expired (is in the past from now)
+ *
+ * @param {Date} date
+ * @returns {boolean}
+ */
+const hasDateExpired = (date = reqParam('date')) => {
+  const now = new Date();
+  
+  if (!isDate(date)) { throw new Error('Param "date" isnt a Date instance'); }
+  
+  return date < now;
+};
+
+module.exports = hasDateExpired;
